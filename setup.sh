5df4493c25d01c9fea72473946d08b7f35e2cd7e
TomWis97 <github@tomwishaupt.nl>
2018-04-26 11:33:18 +0200

    Initial code

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+username=tom
+uid=`id -u`
+if [[ $uid != 0 ]]
+then
+    echo "Run as root"
+    exit 1
+fi
+set -xe
+apk add bash bash-doc bash-completion python3 git vim tmux
+pip3 install --upgrade pip
+adduser -s /bin/bash $username
+userscript="~$username/user_setup.sh"
+cp ~/alpinelinux-config/user_setup.sh "$userscript"
+chown $username $userscript
+chmod 550 $userscript
+su - tom $userscript
+rm $userscript
+echo Done.
---
+++ user_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "################################################"
+echo "# Generating SSH keys. Input required.         #"
+echo "################################################"
+ssh-keygen -b 4096 -f ~/.ssh/id_rsa
+echo 
+echo "################################################"
+echo "# Add this key to GitHub.                      #"
+echo "################################################"
+cat ~/.ssh/id_rsa.pub
+echo "################################################"
+read -p "Press Enter to continue."
+git clone git@github.com:TomWis97/alpinelinux-config.git ~/alpinelinux-config
+pip3 install --user git+https://github.com/powerline/powerline
+ln -s ~/alpinelinux-config/configs/bashrc ~/.bashrc
+ln -s ~/alpinelinux-config/configs/tmux.conf ~/.tmux.conf
+ln -s ~/alpinelinux-config/configs/vimrc ~/.vimrc
