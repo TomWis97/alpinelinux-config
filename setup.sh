2e73f1e161e2e723136708251407c0ebc9ee904b
TomWis97 <github@tomwishaupt.nl>
2018-04-26 13:32:23 +0200

    Add checks

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/ash
+echo $0
+if [[ $0 != "./setup.sh" ]]
+then
+    echo "cd to the git repo first."
+    exit 1
+fi
 username=tom
 uid=`id -u`
 if [[ $uid != 0 ]]
@@ -7,6 +13,8 @@ then
     exit 1
 fi
 set -xe
+apk update
+apk upgrade
 apk add bash bash-doc bash-completion python3 git vim tmux
 pip3 install --upgrade pip
 adduser -s /bin/bash $username
@@ -14,6 +22,7 @@ userscript="/home/$username/user_setup.sh"
 cp ~/alpinelinux-config/user_setup.sh "$userscript"
 chown $username $userscript
 chmod 550 $userscript
+echo Running script as user.
 su - tom $userscript
 rm $userscript
 echo Done.
